4aca22083af19db595cda4c7f5b5e8d4594f7dec
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-02 11:26:58 -0700

    Create fizzbuzz.jl

---
+++ fizzbuzz.jl
@@ -0,0 +1,9 @@
+for i = 1:15
+    if (i % 3) == 0
+        println("$i fizz")
+    elseif (i % 5) == 0
+        println("$i buzz")
+    elseif (i % 5) == 0 && (i % 3) == 0
+        println("$i fizz buzz")
+    end
+end
