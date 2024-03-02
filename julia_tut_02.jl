1028007ba9a77002d040d7b3aa4270add9916e44
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-02 11:56:19 -0700

    Update julia_tut_02.jl

--- julia_tut_02.jl
+++ julia_tut_02.jl
@@ -1,5 +1,18 @@
+using Printf
+
 a6 = [sin, cos, tan]
 
 for f in a6
     println(f(0))
 end
+
+a7 = [1 2 3; 4 5 6]
+for n = 1:2, m = 1:3
+    @printf("%d", a7[n, m])
+end
+
+a10 = [n^2 for n in 1:5]
+println(a10)
+
+a11 = [n *m for n in 1:5, m in 1:5]
+println(a11)
