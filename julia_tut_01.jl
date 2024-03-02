ed21437bb6e63095ed1dddb13e5f38cd4e5b2f81
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-02 11:30:27 -0700

    Update julia_tut_01.jl

--- julia_tut_01.jl
+++ julia_tut_01.jl
@@ -25,3 +25,20 @@ if age >= 5 && age <= 6
 else
     println("Grown up")
 end
+
+for i = 1:5, j = 2:2:10
+    println("$i $j")
+end
+
+a1 = zeros(Int32, 2, 2)
+a2 = Array{Int32}(undef, 5)
+a3 = Float64[]
+a4 = [1, 2, 3]
+
+println(a4[1])
+println(a4[end])
+println(5 in a4)
+println(findfirst(isequal(2), a4))
+f(a) = (a >= 2)
+println(findall(f, a4))
+
