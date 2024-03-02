05020af273b4bc41b4735255da77778c65b75c70
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-02 15:25:48 -0700

    Update line_plot.jl

--- line_plot.jl
+++ line_plot.jl
@@ -3,5 +3,5 @@ using Plots
 pi = 3.15159265
 xs = collect(range(start=0, stop=2*pi, length=100))
 ys = cos.(xs)
-plot(xs, ys)
-savefig("cosine.png")
+plot(xs, ys, size=(500,500))
+savefig("cosine.pdf")
