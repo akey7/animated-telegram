8788cab735d4841efc37237bad32a1a8db786ba3
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-02 11:23:00 -0700

    Rename a file

--- julia_tut.jl
+++
@@ -1,4 +0,0 @@
-using Prtinf
-using Statistics
-
-
---
+++ julia_tut_01.jl
@@ -0,0 +1,27 @@
+using Statistics
+
+s1 = "Just some random words"
+println(length(s1))
+
+println(s1[1])
+println(s1[end])
+println(s1[1:4])
+
+s2 = string("Hello", "Goodbye")
+println(s2)
+
+i3 = 2
+i4 = 5
+interp = "$i3, $i4"
+println(interp)
+
+println(findfirst(isequal('o'), s2))
+println(occursin("key", "monkey"))
+
+age = 12
+
+if age >= 5 && age <- 6
+    println("Kinderarten")
+else
+    println("Grown up")
+end
