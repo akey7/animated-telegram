c6fb49a38e0b78a13ee632e478643f566c54fb75
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-03 01:49:04 -0700

    Update maxwell_boltzmann.jl

--- maxwell_boltzmann.jl
+++ maxwell_boltzmann.jl
@@ -1,3 +1,15 @@
 using Plots
-using Distributions
 
+function maxwell_boltzmann_pdf(velocities, sigma)
+    function mb_pdf(v)
+        sqrt(2 / π) * (v^2 * exp(-v^2 / (2 * sigma^2))) / sigma^3
+    end
+
+    mb_pdf.(velocities)
+end
+
+velocities = collect(range(start=0, stop=2000, length=100))
+pdf = maxwell_boltzmann_pdf(velocities, 300)
+
+display(plot(velocities, pdf))
+readline()
