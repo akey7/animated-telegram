01d7c91885ffec855013462de557a96c893074b3
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-05 19:35:43 -0700

    Create cpu_02.jl

---
+++ gpu/cpu_02.jl
@@ -0,0 +1,16 @@
+using Test
+
+N = 2^20
+x = fill(1.0f0, N)  # a vector filled with 1.0 (Float32)
+y = fill(2.0f0, N)  # a vector filled with 2.0
+
+function sequential_add!(y, x)
+    for i in eachindex(y, x)
+        @inbounds y[i] += x[i]
+    end
+    return nothing
+end
+
+fill!(y, 2)
+sequential_add!(y, x)
+@test all(y .== 3.0f0)
