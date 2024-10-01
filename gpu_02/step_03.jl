adf3bbf126cb7a60dc0ba3c47a297b65155c1a37
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-01 13:22:41 -0600

    Create step_03.jl

---
+++ gpu_02/step_03.jl
@@ -0,0 +1,27 @@
+using Plots
+using BenchmarkTools
+
+function lap2d!(u0, unew0)
+    M, N = size(u0)
+    for j ∈ 2:N-1
+        for i ∈ 2:M-1
+            @inbounds unew0[i, j] = 0.25 * (u0[i+1,j] + u0[i-1,j] + u0[i,j+1] + u0[i,j-1])
+        end
+    end
+end
+
+M = 4096
+N = 4096
+u = zeros(Float64, M, N)
+u[1, :] = u[end, :] = u[:, 1] = u[:, end] .= 10.0
+unew = copy(u)
+
+# for i ∈ 1:50_000
+#     lap2d!(u, unew)
+#     global u = copy(unew)
+# end
+
+@btime lap2d!(u, unew)
+
+display(heatmap(u))
+readline()
