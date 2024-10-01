faa2bc2bff2dc05d9f5b8c9ebff3202d76bde4fc
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-01 13:12:07 -0600

    Update step_01.jl

--- gpu_02/step_01.jl
+++ gpu_02/step_01.jl
@@ -1,8 +1,24 @@
-function lap2d!(u, unew)
-    M, N = size(u)
+using Plots
+
+function lap2d!(u0, unew0)
+    M, N = size(u0)
     for j ∈ 2:N-1
         for i ∈ 2:M-1
-            unew[i, j] = 0.25 * (u[i+1,j] + u[i-1,j] + u[i,j+1] + u[i,j-1])
+            unew0[i, j] = 0.25 * (u0[i+1,j] + u0[i-1,j] + u0[i,j+1] + u0[i,j-1])
         end
     end
 end
+
+M = 4096
+N = 4096
+u = zeros(Float64, M, N)
+u[1, :] = u[end, :] = u[:, 1] = u[:, end] .= 10.0
+unew = copy(u)
+
+for i ∈ 1:50_000
+    lap2d!(u, unew)
+    global u = copy(unew)
+end
+
+heatmap(u)
+readline()
