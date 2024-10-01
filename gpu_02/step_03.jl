230d2d745118e26cd6969848cdaa0668d209ac18
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-01 13:33:01 -0600

    Update step_03.jl

--- gpu_02/step_03.jl
+++ gpu_02/step_03.jl
@@ -4,7 +4,7 @@ using .Threads
 
 function lap2d!(u0, unew0)
     M, N = size(u0)
-    for j ∈ 2:N-1
+    @threads for j ∈ 2:N-1
         for i ∈ 2:M-1
             @inbounds unew0[i, j] = 0.25 * (u0[i+1,j] + u0[i-1,j] + u0[i,j+1] + u0[i,j-1])
         end
@@ -17,12 +17,10 @@ u = zeros(Float64, M, N)
 u[1, :] = u[end, :] = u[:, 1] = u[:, end] .= 10.0
 unew = copy(u)
 
-# for i ∈ 1:50_000
-#     lap2d!(u, unew)
-#     global u = copy(unew)
-# end
-
-@btime lap2d!(u, unew)
+for i ∈ 1:50
+    @btime lap2d!(u, unew)
+    @btime global u = copy(unew)
+end
 
 display(heatmap(u))
 readline()
