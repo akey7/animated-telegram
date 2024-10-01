c24989e86aac7766504f8ea662cf791a0e98d750
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-01 14:19:52 -0600

    Update step_06.jl

---
+++ intro_to_hpc/step_06.jl
@@ -0,0 +1,25 @@
+using CUDA
+using BenchmarkTools
+
+@inbounds function lap2d_gpu!(u, unew)
+    M, N = size(u)
+    j = threadIdx().y + (blockIdx().y - 1) * blockDim().y
+    i = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    if i > 1 && i < N && j > 1 && j < M
+        unew[i, j] = 0.25 * (u[i+1,j] + u[i-1,j] + u[i,j+1] + u[i,j-1])
+    end
+    return nothing
+end
+
+M = 4096
+N = 4096
+threads = (32, 32) # 32*32=1024
+blocks = (N÷threads[1], M÷threads[2])
+
+u = zeros(Float32, M, N)
+u[1, :] = u[end, :] = u[:, 1] = u[:, end] .= 10.0
+unew = copy(u)
+u_d = CuArray(u)
+unew_d = CuArray(unew)
+
+@btime CUDA.@sync @cuda blocks=blocks threads=threads lap2d_gpu2!($u_d, $unew_d)
