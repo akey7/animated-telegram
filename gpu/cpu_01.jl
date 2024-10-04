87488dd8dbd76b44ae3aaba51eef24f039729313
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-03 20:18:22 -0600

    Update cpu_01.jl

--- gpu/cpu_01.jl
+++ gpu/cpu_01.jl
@@ -9,3 +9,5 @@ y = fill(2.0f0, N)  # a vector filled with 2.0
 y .+= x             # increment each element of y with the corresponding element of x
 
 @test all(y .== 3.0f0)
+
+println("No news is good news!")
