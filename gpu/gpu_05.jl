fb718d3baa08b92918b898e55b3fb98866347ec0
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-04 09:47:33 -0600

    Update gpu_05.jl

--- gpu/gpu_05.jl
+++ gpu/gpu_05.jl
@@ -18,6 +18,6 @@ function gpu_add2!(y, x)
 end
 
 fill!(y_d, 2)
-@cuda threads=256 gpu_add2!(y_d, x_d)
+@cuda threads=1024 gpu_add2!(y_d, x_d)  # Example uses 256, but I have 1024 on all my machines
 @test all(Array(y_d) .== 3.0f0)
 println("No news is good news!")
