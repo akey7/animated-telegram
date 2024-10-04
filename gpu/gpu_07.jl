db4113efbc36b17109a9dbf5b13a61433ce62c31
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-04 10:03:44 -0600

    Create gpu_07.jl

---
+++ gpu/gpu_07.jl
@@ -0,0 +1,30 @@
+# From https://cuda.juliagpu.org/stable/tutorials/introduction/
+
+using CUDA
+using Test
+using BenchmarkTools
+
+N = 2^20
+x_d = CUDA.fill(1.0f0, N)  # a vector stored on the GPU filled with 1.0 (Float32)
+y_d = CUDA.fill(2.0f0, N)  # a vector stored on the GPU filled with 2.0
+
+function gpu_add3!(y, x)
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x  
+    for i ∈ index:stride:length(y)
+        @inbounds y[i] += x[i]
+    end
+    return nothing
+end
+
+numblocks = ceil(Int, N/256)
+
+function bench_gpu3!(y, x)
+    numblocks = ceil(Int, length(y)/256)
+    CUDA.@sync begin
+        @cuda threads=256 blocks=numblocks gpu_add3!(y, x)
+    end
+end
+
+fill!(y_d, 2)
+@btime bench_gpu3!($y_d, $x_d)
