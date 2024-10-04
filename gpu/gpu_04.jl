bd036306d595c97168f41ceb9bb3ffa7e00bb088
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-03 20:40:44 -0600

    Create gpu_04.jl

---
+++ gpu/gpu_04.jl
@@ -0,0 +1,22 @@
+# From https://cuda.juliagpu.org/stable/tutorials/introduction/
+
+using CUDA
+using Test
+using BenchmarkTools
+
+N = 2^20
+x_d = CUDA.fill(1.0f0, N)  # a vector stored on the GPU filled with 1.0 (Float32)
+y_d = CUDA.fill(2.0f0, N)  # a vector stored on the GPU filled with 2.0
+
+function gpu_add1!(y, x)
+    for i ∈ eachindex(y)
+        @inbounds y[i] += x[i]
+    end
+    return nothing
+end
+
+fill!(y_d, 2)
+@cuda gpu_add1!(y_d, x_d)
+@test all(Array(y_d) .== 3.0f0)
+
+println("No news is good news!")
