e93065eb92a7b4244f3a76a5574602222ac79e95
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-04 09:44:45 -0600

    Update gpu_05.jl

--- gpu/gpu_05.jl
+++ gpu/gpu_05.jl
@@ -8,11 +8,16 @@ N = 2^20
 x_d = CUDA.fill(1.0f0, N)  # a vector stored on the GPU filled with 1.0 (Float32)
 y_d = CUDA.fill(2.0f0, N)  # a vector stored on the GPU filled with 2.0
 
-function gpu_add1!(y, x)
-    for i ∈ eachindex(y)
+function gpu_add2!(y, x)
+    index = threadIdx().x 
+    stride = blockDim().x 
+    for i ∈ index:stride:length(y)
         @inbounds y[i] += x[i]
     end
     return nothing
 end
 
+fill!(y_d, 2)
+@cuda threads=256 gpu_add2!(y_d, x_d)
+@test all(Array(y_d) .== 3.0f0)
 println("No news is good news!")
