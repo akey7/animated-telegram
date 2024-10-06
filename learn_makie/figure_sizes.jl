2a1a857c564d9a24f5c6cd47f77df52ef0174398
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-06 09:31:28 -0600

    Update figure_sizes.jl

--- learn_makie/figure_sizes.jl
+++ learn_makie/figure_sizes.jl
@@ -1,3 +1,12 @@
 # Following https://docs.makie.org/stable/tutorials/aspect-tutorial
 
+using CairoMakie
+set_theme!(backgroundcolor = :gray90)
 
+f = Figure(size = (800, 500))
+ax = Axis(f[1, 1], aspect = 1)
+Colorbar(f[1, 2])
+colsize!(f.layout, 1, Aspect(1, 1.0))
+Box(f[1, 1], color = (:red, 0.2), strokewidth = 0)
+resize_to_layout!(f)
+save("learn_makie.png", f)
