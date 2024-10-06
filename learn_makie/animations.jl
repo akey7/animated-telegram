c1998105e74ae45c4018317b18e9db3291e6ea65
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-06 10:42:32 -0600

    Update animations.jl

--- learn_makie/animations.jl
+++ learn_makie/animations.jl
@@ -1,3 +1,13 @@
 # https://docs.makie.org/stable/explanations/animation
 
+using GLMakie
+using Makie.Colors
 
+fig, ax, lineplot = lines(0..10, sin; linewidth=10)
+nframes = 30
+framerate = 30
+hue_iterator = range(0, 360, length=nframes)
+
+record(fig, "color_animation.mp4", hue_iterator; framerate=framerate) do hue
+    lineplot.color = HSV(hue, 1, 0.75)
+end
