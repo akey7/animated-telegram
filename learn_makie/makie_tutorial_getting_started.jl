6a454a762c7509735303d1485e4e61f17a77e09d
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-06 07:41:04 -0600

    Update makie_tutorial_getting_started.jl

--- learn_makie/makie_tutorial_getting_started.jl
+++ learn_makie/makie_tutorial_getting_started.jl
@@ -1,9 +1,12 @@
 using CairoMakie
 
 seconds = 0:0.1:2
-measurements = [8.2, 8.4, 6.3, 9.5, 9.1, 10.5, 8.6, 8.2, 10.5, 8.5, 7.2,
-        8.8, 9.7, 10.8, 12.5, 11.6, 12.1, 12.1, 15.1, 14.7, 13.1]
+measurements = [8.2, 8.4, 6.3, 9.5, 9.1, 10.5, 8.6, 8.2, 10.5, 8.5, 7.2, 8.8, 9.7, 10.8, 12.5, 11.6, 12.1, 12.1, 15.1, 14.7, 13.1]
 
-lines(seconds, measurements)
+# Create a simple scatter plot
+fig = Figure()
+ax = Axis(fig[1, 1])
+lines!(ax, 1:10, rand(10))
 
-readline()
+# Display the plot
+display(fig)
