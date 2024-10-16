8cf48c7ffff67ea2762af4b8789962dc9e0f5320
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-16 17:22:45 -0600

    Create production_envelope.jl

---
+++ cobrexa/production_envelope.jl
@@ -0,0 +1,32 @@
+# Following https://cobrexa.github.io/COBREXA.jl/dev/examples/03b-parsimonious-flux-balance/
+
+using COBREXA
+import JSONFBCModels, HiGHS
+import AbstractFBCModels as A
+
+println("############################################################")
+println("# LOAD MODEL                                               #")
+println("############################################################")
+
+model = load_model("e_coli_core.json")
+
+for reaction ∈ A.reactions(model)
+    println(reaction)
+end
+
+println("############################################################")
+println("# PRODUCTION ENVELOPE                                      #")
+println("############################################################")
+
+envelope = objective_production_envelope(
+    model,
+    ["EX_o2_e", "EX_co2_e"];
+    breaks = 5,
+    optimizer = HiGHS.Optimizer,
+)
+
+println("Available data in solution:")
+
+for element ∈ envelope
+    println(element)
+end
