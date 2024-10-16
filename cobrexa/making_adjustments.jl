63297f247144767edaff8c2f9459795bbc984bc7
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-16 09:51:35 -0600

    Update making_adjustments.jl

--- cobrexa/making_adjustments.jl
+++ cobrexa/making_adjustments.jl
@@ -1,6 +1,7 @@
 using COBREXA
 import JSONFBCModels
 import AbstractFBCModels.CanonicalModel as CM
+import HiGHS
 
 model = convert(CM.Model, load_model("e_coli_core.json"))
 
@@ -11,3 +12,9 @@ println("############################################################")
 for (key, value) ∈ model.reactions
     println("Abbreviation: ", key, " Name: ", value.name, ", bounds: ", (value.lower_bound, value.upper_bound))
 end
+
+println("############################################################")
+println("# PFK STOICHIOMETRY                                        #")
+println("############################################################")
+
+println(model.reactions["PFK"].stoichiometry)
