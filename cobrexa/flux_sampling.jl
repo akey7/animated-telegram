cf19e8471e1cc886949eefa77762b806fcc0846f
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-16 08:04:02 -0600

    Update flux_sampling.jl

--- cobrexa/flux_sampling.jl
+++ cobrexa/flux_sampling.jl
@@ -1,6 +1,7 @@
 # Follows https://cobrexa.github.io/COBREXA.jl/dev/examples/06a-sampling/#Flux-sampling
 
 using COBREXA
+using CairoMakie
 import JSONFBCModels, HiGHS
 
 model = load_model("e_coli_core.json")
@@ -12,3 +13,15 @@ s = flux_sample(
     n_chains = 2,
     collect_iterations = [10],
 )
+
+fluxes = [s.O2t s.CO2t]
+
+fig = Figure(resolution = (1080/2, 1080/2))
+ax = Axis(fig[1, 1])
+ax.title = "Scatter Plot"
+ax.xlabel = "O2t"
+ax.ylabel = "CO2t"
+scatter!(ax, fluxes[:, 1], fluxes[:, 2], color = :blue, markersize = 10)
+display(fig)
+
+println("Press enter to exit...")
