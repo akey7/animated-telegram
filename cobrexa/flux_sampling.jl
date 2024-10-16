bba0b5635f6cbf7c2673ffc2b603341b8556f267
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-16 07:54:56 -0600

    Update flux_sampling.jl

--- cobrexa/flux_sampling.jl
+++ cobrexa/flux_sampling.jl
@@ -1,3 +1,14 @@
 # Follows https://cobrexa.github.io/COBREXA.jl/dev/examples/06a-sampling/#Flux-sampling
 
+using COBREXA
+import JSONFBCModels, HiGHS
 
+model = load_model("e_coli_core.json")
+
+s = flux_sample(
+    model,
+    optimizer = HiGHS.Optimizer,
+    objective_bound = relative_tolerance_bound(0.99),
+    n_chains = 2,
+    collect_iterations = [10],
+)
