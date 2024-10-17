9fd703235e7c9af2e6c2156920594f24ef963942
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-17 09:58:10 -0600

    Update thermodynamic.jl

--- cobrexa/thermodynamic.jl
+++ cobrexa/thermodynamic.jl
@@ -86,3 +86,35 @@ mmdf_solution = max_min_driving_force_analysis(
 for element ∈ mmdf_solution
     println(element)
 end
+
+println("############################################################")
+println("# CONSTRAINTS VARIABILITY                                  #")
+println("############################################################")
+
+mmdf_system = max_min_driving_force_constraints(
+    model;
+    reaction_standard_gibbs_free_energies,
+    reference_flux,
+    constant_concentrations = Dict("g3p_c" => exp(-8.5)),
+    concentration_ratios = Dict(
+        "atp" => ("atp_c", "adp_c", 10.0),
+        "nadh" => ("nadh_c", "nad_c", 0.13),
+    ),
+    proton_metabolites = ["h_c"],
+    water_metabolites = ["h2o_c"],
+    concentration_lower_bound = 1e-6, # mol/L
+    concentration_upper_bound = 1e-1, # mol/L
+    T = 298.15, # Kelvin
+    R = 8.31446261815324e-3, # kJ/K/mol
+)
+
+cva_solution = constraints_variability(
+    mmdf_system,
+    mmdf_system.log_concentrations,
+    objective = mmdf_system.min_driving_force.value,
+    optimizer = HiGHS.Optimizer,
+)
+
+for element ∈ cva_solution
+    println(element)
+end
